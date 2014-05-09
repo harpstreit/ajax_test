a04c9dc7ddcae730000955ae9050d1fdb72f858c
Ryan <ryan.joshua.jones@gmail.com>
2014-05-09 15:08:16 -0700

    name change

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res) {
-  res.render('index', { title: 'Crystal\'s test of truth' });
+  res.render('index', { title: 'ajax test of truth' });
 };
 
 exports.api = require('./api.js');
