43141195586b2dbe9230f038ed3072199882ecc9
Crystal <crystal.harpstreit@gmail.com>
2014-05-20 10:14:11 -0700

    A work in progress.

--- client/js/main.js
+++ client/js/main.js
@@ -7,4 +7,20 @@
     console.log('all hooked up');
   });
 
+  function display(cats) {
+    $("ul.cats").append("<li>" + cats + "</li>");
+  }
+
+  function fetch() {
+    $.ajax({
+      type: "GET",
+      url: "cats/api",
+      success: function()
+    })
+  }
+
+  function addListeners()
+  $("button").click function() {
+
+  }
 }());
